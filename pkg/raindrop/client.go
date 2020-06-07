729ee0755fdc039fb4d3959b9d02d4bef1761691
ytakahashi <yt01tc@gmail.com>
2020-06-07 16:49:33 +0900

    Get tags API (#6)

--- pkg/raindrop/client.go
+++ pkg/raindrop/client.go
@@ -1,3 +1,6 @@
+// Package raindrop implements Raindrop.io API client.
+//
+// API Reference: https://developer.raindrop.io/
 package raindrop
 
 import (
@@ -51,6 +54,18 @@ type Raindrop struct {
 type Raindrops struct {
 	Result bool       `json:"result"`
 	Items  []Raindrop `json:"items"`
+}
+
+// Tag represents get tags api response item
+type Tag struct {
+	ID    string `json:"_id"`
+	Count int    `json:"count"`
+}
+
+// Tags represents get tags api response
+type Tags struct {
+	Result bool  `json:"result"`
+	Items  []Tag `json:"items"`
 }
 
 // NewClient creates Raindrop Client
@@ -109,6 +124,61 @@ func (c *Client) GetRaindrops(collectionID string) (*Raindrops, error) {
 	}
 
 	return r, nil
+}
+
+// GetTags calls Get tags API.
+// Reference: https://developer.raindrop.io/v1/tags#get-tags
+func (c *Client) GetTags() (*Tags, error) {
+	path := "/rest/v1/tags"
+	request, err := c.newRequest("GET", path)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := c.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	r := new(Tags)
+	if err := parseResponse(response, 200, &r); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// GetTaggedRaindrops finds raindrops with exact tags.
+// This function calls Get raindrops API with collectionID=0 and specify given tag as a search parameter.
+//
+// Reference: https://developer.raindrop.io/v1/raindrops/multiple#search-parameter
+func (c *Client) GetTaggedRaindrops(tag string) (*Raindrops, error) {
+	path := "/rest/v1/raindrops/0"
+	request, err := c.newRequest("GET", path)
+	if err != nil {
+		return nil, err
+	}
+
+	params := request.URL.Query()
+	searchParameter := createSingleSearchParameter("tag", tag)
+	params.Add("search", searchParameter)
+	request.URL.RawQuery = params.Encode()
+
+	response, err := c.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	r := new(Raindrops)
+	if err := parseResponse(response, 200, &r); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func createSingleSearchParameter(k, v string) string {
+	return fmt.Sprintf(`[{"key":"%s","val":"%s"}]`, k, v)
 }
 
 func (c *Client) newRequest(method string, apiPath string) (*http.Request, error) {
--- pkg/raindrop/client_test.go
+++ pkg/raindrop/client_test.go
@@ -128,6 +128,106 @@ func Test_GetCollections(t *testing.T) {
 	}
 }
 
+func Test_GetTags(t *testing.T) {
+	// Given
+	tag1 := Tag{
+		ID:    "tag 1",
+		Count: 10,
+	}
+	tag2 := Tag{
+		ID:    "tag 2",
+		Count: 100,
+	}
+	expected := Tags{
+		Result: true,
+		Items:  []Tag{tag1, tag2},
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res, err := json.Marshal(expected)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	}))
+
+	defer ts.Close()
+
+	// When
+	sut := createTestClient(ts, t)
+
+	// Then
+	actual, err := sut.GetTags()
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+	if actual.Result != true {
+		t.Error("actual.Result")
+	}
+	if len(actual.Items) != 2 {
+		t.Errorf("Unexpected length: %d", len(actual.Items))
+	}
+	if !reflect.DeepEqual(actual.Items[0], tag1) {
+		t.Errorf("Unexpected: %v, %v", actual.Items[0], tag1)
+	}
+	if !reflect.DeepEqual(actual.Items[1], tag2) {
+		t.Errorf("Unexpected: %v, %v", actual.Items[1], tag2)
+	}
+}
+
+func Test_GetTaggedRaindrops(t *testing.T) {
+	// Given
+	raindrop1 := Raindrop{
+		Tags:  []string{"foo", "bar"},
+		Title: "Test 1",
+		Link:  "https://example.com/1",
+	}
+	expected := Raindrops{
+		Result: true,
+		Items:  []Raindrop{raindrop1},
+	}
+	tag := "tag 1"
+	expectedQuery := `[{"key":"tag","val":"tag 1"}]`
+	var actualQuery string
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res, err := json.Marshal(expected)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		actualQuery = r.URL.Query().Get("search")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	}))
+
+	defer ts.Close()
+
+	// When
+	sut := createTestClient(ts, t)
+
+	// Then
+	actual, err := sut.GetTaggedRaindrops(tag)
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+	if actualQuery != expectedQuery {
+		t.Errorf("Unexpected: %v, %v", actualQuery, expectedQuery)
+	}
+	if actual.Result != true {
+		t.Error("actual.Result")
+	}
+	if len(actual.Items) != 1 {
+		t.Errorf("Unexpected length: %d", len(actual.Items))
+	}
+	if !reflect.DeepEqual(actual.Items[0], raindrop1) {
+		t.Errorf("Unexpected: %v, %v", actual.Items[0], raindrop1)
+	}
+}
+
 func createTestClient(ts *httptest.Server, t *testing.T) Client {
 	u, err := url.Parse(ts.URL)
 	if err != nil {
